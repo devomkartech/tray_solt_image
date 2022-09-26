d75e740c590378bd9a8d06bfa366384d506ae2ed
7omkar <53603522+7omkar@users.noreply.github.com>
2022-09-26 21:20:09 +0530

    update

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ import numpy as np
 
 #video or img Read
 
-cap = cv2.VideoCapture(r"H:\tray_solt_image\image\carPark.mp4")
+cap = cv2.VideoCapture(r"H:\tray_solt_image\image\video1.mp4")
+
+with open('ImgSlot', 'rb') as f:
+        posList = pickle.load(f)
+
+width, height =  80, 575
 
 while True:
-    success, img = cap.read()
+
+
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+    success, img = cap.read()    
+
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height),(255,0,255),2)    
+
+    
     cv2.imshow("Image", img)
-    cv2.waitKey(30)
+    cv2.waitKey(10)
